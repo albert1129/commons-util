60000457ef1e1754a40cf87935f445109e4cfb0a
albert <albert@DESKTOP-Q497AV3>
2017-08-08 21:17:59 +0800

    update

--- commons-util/src/main/java/com/ogsu/commons/util/TestConfilct.java
+++ commons-util/src/main/java/com/ogsu/commons/util/TestConfilct.java
@@ -3,7 +3,7 @@ package com.ogsu.commons.util;
 public class TestConfilct {
 	
 	public static void main(String[] args) {
-		System.err.println("sdfsgdgdfsdfs");
+		System.err.println("sdgtjthdft");
 		System.err.println("sdfsgdgdfsdfs");
 		System.err.println("sdfsgdgdfsdfs");
 	}

d4e684044467354a0a7676858e0090fb2319edb9
albert1129 <384092360@qq.com>
2017-08-08 21:25:32 +0800

    Update TestConfilct.java

--- commons-util/src/main/java/com/ogsu/commons/util/TestConfilct.java
+++ commons-util/src/main/java/com/ogsu/commons/util/TestConfilct.java
@@ -3,8 +3,8 @@ package com.ogsu.commons.util;
 public class TestConfilct {
 	
 	public static void main(String[] args) {
-		boolean result = false;
-		if(result){
+		boolean xx = false;
+		if(xx){
 			System.err.println("正确");
 		}else{
 			System.err.println("错误");

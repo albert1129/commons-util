c1595f344a9851e493f3c9495717192ea218ab1e
albert <albert@DESKTOP-Q497AV3>
2017-08-08 21:25:08 +0800

    update

--- commons-util/src/main/java/com/ogsu/commons/util/TestConfilct.java
+++ commons-util/src/main/java/com/ogsu/commons/util/TestConfilct.java
@@ -3,9 +3,12 @@ package com.ogsu.commons.util;
 public class TestConfilct {
 	
 	public static void main(String[] args) {
-		System.err.println("sdgtjthdft");
-		System.err.println("sdfsgds");
-		System.err.println("sdfsgdgdfsdfs");
+		boolean result = false;
+		if(result){
+			System.err.println("正确");
+		}else{
+			System.err.println("错误");
+		}
 	}
 
 }
